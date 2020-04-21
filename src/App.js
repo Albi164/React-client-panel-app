209355cbf47078a55cafb1f2349465b3e4c9f15c
Albi Mrázová <mrazova.albina@gmail.com>
2020-04-21 21:44:20 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import './App.css';
+import AppNavBar from './components/layout/AppNavBar';
+import Dashboard from './components/layout/Dashboard';
+
+import {Provider} from 'react-redux';
+import store from './store';
+
+function App() {
+  return (
+    <Provider store={store}>
+    <Router>
+    <div className="App">
+      <AppNavBar/>
+      <div className="container">
+        <Switch>
+          <Route exact path="/" component={Dashboard}/>
+        </Switch>
+      </div>
+    </div>
+    </Router>
+    </Provider>
+  );
+}
+
+export default App;
